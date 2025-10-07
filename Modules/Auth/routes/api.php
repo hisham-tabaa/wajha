71daa570677c2dda0483e076b1bd936da1fbcf3e
Rania Boushy <raniaboushy@gmail.com>
2025-10-07 15:34:08 -0700

    fix rania

--- Modules/Auth/routes/api.php
+++ Modules/Auth/routes/api.php
@@ -13,6 +13,6 @@ Route::post('login', [LoginController::class, 'login']);
 // Keep the original route for backwards compatibility (role defaults to "default")
 Route::post('auth/google', [GoogleAuthController::class, 'login']);
 //
-    Route::post('/register-users', [UserRegisterController::class, 'register']);
+Route::post('/register-users', [UserRegisterController::class, 'register']);
 
 
