ee9c6df26fcab5de6580dd719c8288eae78f27ca
MohAwad <muhamedawad2000@gmail.com>
2025-10-14 17:01:50 +0300

    delete modules

--- Modules/Listing/routes/web.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-use Modules\Listing\Http\Controllers\ListingController;
-
-Route::middleware(['auth', 'verified'])->group(function () {
-    Route::resource('listings', ListingController::class)->names('listing');
-});
