04c2d32145ba5053643ad1f7d3acf960cc84799a
MohAwad <muhamedawad2000@gmail.com>
2025-10-15 19:28:50 +0300

    Merge branch 'development' into Managment/RoleAndPermissions

--- Modules/Auth/app/Mail/SendVerificationCode.php
+++ Modules/Auth/app/Mail/SendVerificationCode.php
@@ -1,35 +1,43 @@
 <?php
-namespace  Modules\Auth\Mail;
 
+namespace Modules\Auth\Mail;
+
+use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Mail\Mailable;
+use Illuminate\Support\Facades\Log;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Contracts\Queue\ShouldQueue;
 
-class Sendverificationcode extends Mailable implements ShouldQueue
+class SendVerificationCode extends Mailable implements ShouldQueue
 {
-    use Queueable, SerializesModels;
+  use Queueable, SerializesModels;
 
   public string $code;
   public string $email;
 
-    public function __construct(string $code, string $email)
-    {
+  public function __construct(string $code, string $email)
+  {
     $this->code = $code;
     $this->email = $email;
-    
+  }
+
+  public function build()
+  {
+    try {
+      return $this->subject('رمز التحقق من البريد الإلكتروني')
+        ->view('auth::emails.verification-code')
+        ->with([
+          'code' => $this->code,
+          'email' => $this->email,
+        ]);
+    } catch (Exception $e) {
+      Log::error('Send Verification Code::MAIL', [
+        'Message' => $e->getMessage(),
+        'File' => $e->getFile(),
+        'Line' => $e->getLine(),
+      ]);
+      throw $e;
     }
-
-    public function build()
-    {
-    return $this->subject('Your Verification Code')
-    ->view('auth::emails.verification-code')
-    ->with([
-        'code' => $this->code,
-        'email' => $this->email,
-    ]);
-
-        }
-
-   
+  }
 }
