53f8c7b24392b146cc62ff610b873168ef80882b
muhamed.awad <muhamed.awad@dev.abjadlms.net>
2025-12-13 15:57:46 +0300

    Revert "Merge pull request #34 from Moh-3wad/vehicels/abd"

--- Modules/Auth/app/Http/Requests/LoginRequest.php
+++ Modules/Auth/app/Http/Requests/LoginRequest.php
@@ -16,6 +16,7 @@ class LoginRequest extends FormRequest
         return [
             'email' => 'required|email|max:255',
             'password' => 'required|string|min:8',
+            
         ];
     }
 
